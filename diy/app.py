b653ee82b05cf22ab6ec33434320a795bc754575
renaud <renaud@apache.org>
2014-11-14 15:59:06 +0100

    new virtualenv

--- diy/app.py
+++ diy/app.py
@@ -3,7 +3,7 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world2")
+        self.write("Hello, world3")
 
 application = tornado.web.Application([
     (r"/", MainHandler),
